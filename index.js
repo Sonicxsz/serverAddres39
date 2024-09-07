d9f8d5a93cba09aeadff32302efdfad38fb8fbd9
arbiu <arbih@mail.ru>
2024-09-07 14:40:31 +0100

    Добавил отправку копи

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const bodyParser = require('body-parser');
 const {mail} = require('./main');
 const apiRouter = require('./controller/api')
 const port = 3001;
+const TelegramApi = require('node-telegram-bot-api')
+
+const {TELE_TOKEN} = process.env
+
+const bot  = new TelegramApi(TELE_TOKEN, {polling: true})
+let chatId;
+bot.on('message', (msg) =>{
+  const text = msg.text;
+  console.log('chatId:', msg.chat.id)
+
+  })
 
 const app = express();
 app.use(cors());
@@ -15,7 +26,8 @@ app.use('/api/v1', apiRouter)
 
 
 app.post('/mail', async(req, res) => {
-  const {order, type} = req.body
+  const {order, telegrammOrder, type} = req.body
+  await bot.sendMessage('-4548345225', telegrammOrder, { parse_mode: 'HTML' })
   return res.json({res: await mail.send(order, type)})
 })  
 
