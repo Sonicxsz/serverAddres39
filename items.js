cd1b8d91d68c1c120d45896ae33740c7457815e5
arbiu <arbih@mail.ru>
2025-06-07 15:52:21 +0100

    update

--- items.js
+++ items.js
@@ -93,7 +93,7 @@ const items = [
         price: 750,
         count: 1,
         grams: 210,
-        img: "/img/assets/season/1.jpg",
+        img: "/assets/img/season/1.jpg",
       },
       {
         id: 'ip11',
@@ -102,7 +102,7 @@ const items = [
         price: 870,
         count: 1,
         grams: 200,
-        img: "/img/assets/season/2.jpg",
+        img: "/assets/img/season/2.jpg",
       },
       ]
   },
